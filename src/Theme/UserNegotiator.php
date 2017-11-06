4424c6ce6ee9eeb77f8ca600815246b816c9614e
Misha Chitharanjan <mishac@mishac.com>
2017-11-06 11:44:14 -0500

    initial commit

---
+++ src/Theme/UserNegotiator.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user_login_admin_theme\Theme\UserNegotiator.
+ */
+
+namespace Drupal\user_login_admin_theme\Theme;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\Theme\ThemeNegotiatorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Switch Theme for user pages.
+ */
+class UserNegotiator implements ThemeNegotiatorInterface, ContainerInjectionInterface {
+  protected $configFactory;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+   public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $route_name = $route_match->getRouteName();
+
+    // Use admin theme for login etc
+    return substr($route_name, 0, 4 ) === 'user' || substr($route_name, 0, 11 ) === 'entity.user';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    $config = $this->configFactory->get('system.theme');
+    return $config->get('admin');
+  }
+}
